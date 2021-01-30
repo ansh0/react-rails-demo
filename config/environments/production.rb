424b28723489859782d370abe1895caed16e05dd
neerajkdhariwal88 <neerajkdhariwal88@gmail.com>
2021-01-30 12:29:19 +0530

    production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,9 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
+  config.assets.complie = true
 
+  config.serve_static_assests = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
